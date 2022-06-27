6f6715a126a4ed730c055248aaf579b18e117f75
Scott Thomas <swt30@users.noreply.github.com>
2022-06-27 21:34:59 +0100

    Run nixfmt

--- flake.nix
+++ flake.nix
@@ -1,55 +1,48 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    rchitect.url = "github:randy3k/rchitect/v0.3.36";
+    rchitect.flake = false;
 
-    py-rchitect.url = "github:randy3k/rchitect/v0.3.36";
-    py-rchitect.flake = false;
-
-    py-radian.url = "github:randy3k/radian/v0.6.3";
-    py-radian.flake = false;
+    radian.url = "github:randy3k/radian/v0.6.3";
+    radian.flake = false;
   };
 
-  outputs = { self, nixpkgs, flake-utils, py-rchitect, py-radian }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages = rec {
-        rchitect = pkgs.python3Packages.buildPythonPackage {
-          pname = "rchitect";
-          version = "0.3.36";
-          src = py-rchitect;
+  outputs = { self, nixpkgs, flake-utils, rchitect, radian }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages = {
+          rchitect = pkgs.python3Packages.buildPythonPackage {
+            pname = "rchitect";
+            version = "0.3.36";
+            src = rchitect;
 
-          preBuild = "export HOME=$TMP";
-          buildInputs = with pkgs.python3Packages; [
-            pytest-runner
-          ];
-          propagatedBuildInputs = with pkgs.python3Packages; [
-            pkgs.R
-            cffi
-            six
-          ];
-          doCheck = false;
+            preBuild = "export HOME=$TMP";
+            buildInputs = with pkgs.python3Packages; [ pytest-runner ];
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              pkgs.R
+              cffi
+              six
+            ];
+            doCheck = false;
+          };
+
+          radian = pkgs.python3Packages.buildPythonApplication {
+            pname = "radian";
+            version = "0.6.3";
+            src = radian;
+
+            preBuild = "export HOME=$TMP";
+            buildInputs = with pkgs.python3Packages; [ pytest-runner ];
+            propagatedBuildInputs = with pkgs.python3Packages; [
+              self.packages.${system}.rchitect
+              prompt_toolkit
+              pygments
+            ];
+            doCheck = false;
+          };
+
+          default = self.packages.${system}.radian;
         };
-
-        radian = pkgs.python3Packages.buildPythonApplication {
-          pname = "radian";
-          version = "0.6.3";
-          src = py-radian;
-
-          preBuild = "export HOME=$TMP";
-          buildInputs = with pkgs.python3Packages; [
-            pytest-runner
-          ];
-          propagatedBuildInputs = with pkgs.python3Packages; [
-            self.packages.${system}.rchitect
-            prompt_toolkit
-            pygments
-          ];
-          doCheck = false;
-        };
-
-        default = radian;
-      };
-    });
+      });
 }
